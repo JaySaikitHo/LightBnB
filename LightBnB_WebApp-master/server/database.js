8fcbba73cd45ab4e4e1d54af70a7c8cb6feb083d
vagrant <ttjho@hotmail.com>
2020-10-15 20:12:01 +0000

    change getAllReservations with pool.query to get data from PSQL lightbnb database

--- LightBnB_WebApp-master/server/database.js
+++ LightBnB_WebApp-master/server/database.js
@@ -59,7 +59,16 @@ exports.addUser = addUser;
  * @return {Promise<[{}]>} A promise to the reservations.
  */
 const getAllReservations = function(guest_id, limit = 10) {
-  return getAllProperties(null, 2);
+  return pool.query(`SELECT properties.*, reservations.*, AVG(property_reviews.rating) AS average_rating
+  FROM properties
+  JOIN reservations ON properties.id = reservations.property_id
+  JOIN property_reviews ON properties.id = property_reviews.property_id
+  WHERE reservations.guest_id = $1
+  GROUP BY properties.id, reservations.id
+  HAVING now()::date > reservations.end_date 
+  ORDER BY reservations.start_date ASC
+  LIMIT $2;`,[guest_id,limit])
+  .then(res => res.rows)
 }
 exports.getAllReservations = getAllReservations;
 
